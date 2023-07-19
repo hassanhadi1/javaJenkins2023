94d4af5d2c31d92d7c2fdd98da94d451a771b3c9
Hadi HASSAN <84808314+hassanhadi1@users.noreply.github.com>
2023-07-19 15:54:23 +0200

    Create App.java

---
+++ App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args){
+        System.out.println("Bonjour, World Devops !");
+        Additionnner(1, 2);
+    }
+
+    public static int Additionnner(int a, int b){
+        return (a + b);
+    }
+}
