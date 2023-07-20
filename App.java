d24d371ccaa0677fec06dd3b9070d66c0e8ab364
hadi <hhadihasan288@gmail.com>
2023-07-20 14:52:08 +0200

    modification test2

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args){
-        System.out.println("Bonjour, World Devops test !");
+        System.out.println("Bonjour, World Devops test test2!");
         Additionnner(1, 2);
     }
 
