6eea34e46c650d4e0d5a7de61984c7727275e759
Hadi HASSAN <84808314+hassanhadi1@users.noreply.github.com>
2023-07-20 14:58:03 +0200

    Update App.java

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args){
-        System.out.println("Bonjour, World Devops test test2!");
+        System.out.println("Bonjour, World Dekgkhgvops test test2!");
         Additionnner(1, 2);
     }
 
